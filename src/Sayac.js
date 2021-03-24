fe5dfa1f206418de28fe8f4e8a0179c8480a15e3
acarerisa <acarerisa84@gmail.com>
2021-03-24 16:58:52 +0300

    complex state işlemi

--- src/Sayac.js
+++ src/Sayac.js
@@ -1,15 +1,24 @@
 import React, {useReducer} from 'react'
 
-const initialState = 0
+const initialState = {
+    ilkSayac:0,
+    ikinciSayac:10
+}
 
 const reducer = (state,action)=>{
-    switch(action){
+    switch(action.type){
         case 'artir':
-            return state + 1
+            return {...state,ilkSayac:state.ilkSayac + action.value}
+        case 'artir-2':
+            return {...state,ikinciSayac:state.ikinciSayac + action.value}
         case 'azalt':
-            return state - 1
+            return {...state,ilkSayac:state.ilkSayac - action.value}
+        case 'azalt-2':
+            return {...state,ikinciSayac:state.ikinciSayac - action.value}
         case 'reset':
             return initialState
+        default:
+            return state
     }
 }
 
@@ -17,10 +26,17 @@ function Sayac() {
     const [sayac,dispatch] = useReducer(reducer,initialState)
     return (
         <div>
-            <div>Sayac:{sayac}</div>
-           <button onClick={()=>dispatch('artir')} >Artır</button> 
-           <button onClick={()=>dispatch('azalt')} >Azalt</button> 
-           <button onClick={()=>dispatch('reset')} >Reset</button> 
+            <div>İlk Sayac:{sayac.ilkSayac}</div>
+            <div>İkinci Sayac:{sayac.ikinciSayac}</div>
+           <button onClick={()=>dispatch({type:'artir', value:1})} >Artır</button> 
+           <button onClick={()=>dispatch({type:'artir', value:5})} >5 Artır</button> 
+           <button onClick={()=>dispatch({type:'artir-2', value:1})} >Artır-ikinci</button> 
+           <button onClick={()=>dispatch({type:'artir-2', value:5})} >5 Artır-ikinci</button> 
+           <button onClick={()=>dispatch({type:'azalt',value:1})} >Azalt</button> 
+           <button onClick={()=>dispatch({type:'azalt',value:5})} >5 Azalt</button> 
+           <button onClick={()=>dispatch({type:'azalt-2',value:1})} >Azalt-ikinci</button> 
+           <button onClick={()=>dispatch({type:'azalt-2',value:5})} >5 Azalt-ikinci</button> 
+           <button onClick={()=>dispatch({type:'reset'})} >Reset</button> 
         </div>
     )
 }
