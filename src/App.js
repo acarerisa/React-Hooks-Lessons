fd929b3aa8478c4ed417879e73d1896b2840906b
acarerisa <acarerisa84@gmail.com>
2021-03-27 14:55:07 +0300

    uygulama

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Bilgiler from "./Components/Bilgiler";
+import Kitaplar from "./Components/Kitaplar";
 import Sayac from "./Components/Sayac";
 
 
 function App() {
   return (
     <div className="App">
-     <Bilgiler/>
+     <Kitaplar/>
     </div>
   );
 }
