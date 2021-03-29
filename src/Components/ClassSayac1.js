e39c16f3509d6028ed5849a47fa3a7d67b038dcf
acarerisa <acarerisa84@gmail.com>
2021-03-29 21:26:46 +0300

    koşullu useEffect işlemi

--- src/Components/ClassSayac1.js
+++ src/Components/ClassSayac1.js
@@ -6,20 +6,26 @@ export default class ClassSayac1 extends Component {
 
     this.state = {
       sayac: 0,
+      isim:""                   
     };
   }
   componentDidMount() {
     document.title = `${this.state.sayac} defa tıklandı`;
   }
 
-  componentDidUpdate() {
-    document.title = `${this.state.sayac} defa tıklandı`;
+  componentDidUpdate(prevProps,prevState) {  
+    if(prevState.sayac !== this.state.sayac){
+      console.log("güncelleme yapıldı")    
+      document.title = `${this.state.sayac} defa tıklandı`;
+    }
+    
   }
 
   render() {
     return (
       <div>
-        <button onClick={() => this.setState({ sayac: this.state.sayac + 1 })}>
+        <input type="text" value={this.state.isim} onChange={(e)=>this.setState({isim:e.target.value})} />
+       <button onClick={() => this.setState({ sayac: this.state.sayac + 1 })}>
           {this.state.sayac} defa tıklandı
         </button>
       </div>
--- src/Components/HookSayac1.js
+++ src/Components/HookSayac1.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react'
 
 export default function HookSayac1() {
     const [sayac, setSayac] = useState(0)
+    const [isim,setIsim] = useState("")
 
     useEffect(()=>{
+        console.log("güncelleme yapıldı")
         document.title=`${sayac} defa tıklandı`
-    })
+    },[sayac])
 
     return (
         <div>
+            <input type="text" value={isim} onChange={(e)=>setIsim(e.target.value)} />
            <button onClick={()=>setSayac(sayac + 1)} >{sayac} defa tıklandı</button> 
         </div>
     )
